cb291243ebe42f280421abd39f5b7057786a0c3d
Shams Nahid <b.m.shamsnahid@gmail.com>
2022-10-24 21:11:22 +0600

    add serialize and deserialize to sessions

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,8 +1,40 @@
+import { client } from '$services/redis';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import { usersKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => {
+	const user = await client.hGetAll(usersKey(id));
+	return deserialize(id, user);
+};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+export const createUser = async (attrs: CreateUserAttrs) => {
+	const id = genId();
+
+	await client.hSet(
+		usersKey(id),
+		serialize({
+			username: attrs.username,
+			password: attrs.password
+		})
+	);
+
+	return id;
+};
+
+const serialize = (user: CreateUserAttrs) => {
+	return {
+		username: user.username,
+		password: user.password
+	};
+};
+
+const deserialize = (id: string, user: { [key: string]: string }) => {
+	return {
+		id,
+		username: user.username,
+		password: user.password
+	};
+};
